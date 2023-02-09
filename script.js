6dc9cd91cd21d227d83310c8f013d7713f33ff13
tyc007 <tonychan550@gmail.com>
2023-02-09 15:25:04 -0800

    Basic Html and Grid Layout

---
+++ script.js
@@ -0,0 +1,17 @@
+const GRIDHEIGHT = 16;
+const GRIDLENGTH = 16;
+
+function createGrid(){
+    let container = document.createElement("div");
+    container.id = "container";
+    document.body.appendChild(container);
+    for (let i = 0; i < GRIDHEIGHT; i++){
+        let div = document.createElement("div");
+        div.id = i;
+        div.className = "grid";
+        container.appendChild(div);
+    }
+    
+}
+
+createGrid();
